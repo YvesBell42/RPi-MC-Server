fe9c12be7cd09b36c84b965b980e7987b4cbe761
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-03 03:56:57 +0000

    Update save.sh

--- save.sh
+++ save.sh
@@ -7,29 +7,28 @@ BackupScript=<PATH_TO_BACKUP_SCRIPT>
 # Check if minecraft server process is found in screen terminals.
 if ! sudo screen -list | grep -q minecraft
 then
-	# Server is not running.
-	# Call backup script.
+    # Server is not running.
+    # Call backup script.
     sudo bash $BackupScript
-	# Server has likely crashed if not running so report.
+    # Server has likely crashed if not running so report.
     date >> "$TempDir/crashes.txt"
-	# Copy RAM Disk server files to persistent server folder.
+    # Copy RAM Disk server files to persistent server folder.
     sudo cp -R $TempDir/* $PermDir/
-	# Restart just in case as server no running anyway.
+    # Restart just in case as server no running anyway.
     sudo reboot
-	
-	# Backup is made before saving files to persistent because if the server
-	# has crashed then there is a possibility of corruption.
+    # Backup is made before saving files to persistent because if the server
+    # has crashed then there is a possibility of corruption.
 else
-	# Server is running.
-	# Use screen to tell players that the server is saving.
+    # Server is running.
+    # Use screen to tell players that the server is saving.
     sudo screen -r minecraft -X stuff "say Saving...$(printf '\r')"
-	# Use screen to toggle saving.
+    # Use screen to toggle saving.
     sudo screen -r minecraft -X stuff "save-off$(printf '\r')"
     sudo screen -r minecraft -X stuff "save-all$(printf '\r')"
-	# Wait 10 seconds for save to finish.
+    # Wait 10 seconds for save to finish.
     sleep 10s
-	# Copy RAM Disk server files to persistent server folder.
+    # Copy RAM Disk server files to persistent server folder.
     sudo cp -R $TempDir/* $PermDir/
-	# Use screen to toggle saving.
+    # Use screen to toggle saving.
     sudo screen -r minecraft -X stuff "save-on$(printf '\r')"
 fi
