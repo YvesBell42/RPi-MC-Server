8b4731e290033db6a1e9585bc163fc3bff80b80c
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-19 23:52:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Change these directory paths according to your setup.
-TempDir=<PATH_TO_RAM_DISK_SERVER_FOLDER>
-PermDir=<PATH_TO_PERSISTENT_SERVER_FOLDER>
-StartScript=<PATH_TO_START_SCRIPT>
+RAM_DiskDir=/home/"$USER"/RPi-MC-Server/RAM_Disk
+PersistentDir=/home/"$USER"/RPi-MC-Server/Persistent
+StartScript=/home/"$USER"/RPi-MC-Server/start.sh
 
 # Copy files from persistent server folder to RAM Disk server folder.
-sudo cp -R $PermDir/* $TempDir/
+sudo cp -R $PersistentDir/* $RAM_DiskDir/
 
 # Call start script for server.
 sudo bash $StartScript
