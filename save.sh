d340e6baa272f1f0a43ab0504994f9e660b53535
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-19 23:51:12 +0000

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Change these directory paths according to your setup.
-TempDir=<PATH_TO_RAM_DISK_SERVER_FOLDER>
-PermDir=<PATH_TO_PERSISTENT_SERVER_FOLDER>
-BackupScript=<PATH_TO_BACKUP_SCRIPT>
+RAM_DiskDir=/home/"$USER"/RPi-MC-Server/RAM_Disk
+PersistentDir=/home/"$USER"/RPi-MC-Server/Persistent
+BackupScript=/home/"$USER"/RPi-MC-Server/backup.sh
 
 # Check if minecraft server process is found in screen terminals.
 if ! sudo screen -list | grep -q minecraft
@@ -11,9 +10,9 @@ then
     # Call backup script.
     sudo bash $BackupScript
     # Server has likely crashed if not running so report.
-    date >> "$TempDir/crashes.txt"
+    date >> "$RAM_DiskDir/crashes.txt"
     # Copy RAM Disk server files to persistent server folder.
-    sudo cp -R $TempDir/* $PermDir/
+    sudo cp -R $RAM_DiskDir/* $PersistentDir/
     # Restart just in case as server no running anyway.
     sudo reboot
     # Backup is made before saving files to persistent because if the server
@@ -28,7 +27,7 @@ else
     # Wait 10 seconds for save to finish.
     sleep 10s
     # Copy RAM Disk server files to persistent server folder.
-    sudo cp -R $TempDir/* $PermDir/
+    sudo cp -R $RAM_DiskDir/* $PersistentDir/
     # Use screen to toggle saving.
     sudo screen -r minecraft -X stuff "save-on$(printf '\r')"
 fi
