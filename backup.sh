906a0277c88bef3984b6e85bf18cca0812272bae
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-19 23:46:48 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Change these directory paths according to your setup.
-BackupDir=<PATH_TO_BACKUPS_FOLDER>
-PermDir=<PATH_TO_PERSISTENT_SERVER_FOLDER>
+BackupDir=/home/"$USER"/RPi-MC-Server/Backups
+PersistentDir=/home/"$USER"/RPi-MC-Server/Persistent
 
 # Zip persistent server files with time and date.
-sudo zip -r "$BackupDir/world-$(date +%M-%H-%d-%m-%Y).zip" "$PermDir/world"
+sudo zip -r "$BackupDir/world-$(date +%M-%H-%d-%m-%Y).zip" "$PersistentDir/world"
 
