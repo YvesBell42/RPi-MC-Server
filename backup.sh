2e92e53e725412b3d393f8529f9534ea4b5534eb
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-20 00:36:09 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-BackupDir=/home/"$USER"/RPi-MC-Server/Backups
-PersistentDir=/home/"$USER"/RPi-MC-Server/Persistent
+BackupDir=/home/"$(whoami)"/RPi-MC-Server/Backups
+PersistentDir=/home/"$(whoami)"/RPi-MC-Server/Persistent
 
 # Zip persistent server files with time and date.
 sudo zip -r "$BackupDir/world-$(date +%M-%H-%d-%m-%Y).zip" "$PersistentDir/world"
