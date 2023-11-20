52d2aa852eaef7e5cf6529d98a0f7ccb9bfecdb2
YvesBell42 <63612338+YvesBell42@users.noreply.github.com>
2023-11-20 00:37:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-RAM_DiskDir=/home/"$USER"/RPi-MC-Server/RAM_Disk
-PersistentDir=/home/"$USER"/RPi-MC-Server/Persistent
-StartScript=/home/"$USER"/RPi-MC-Server/start.sh
+RAM_DiskDir=/home/"$(whoami)"/RPi-MC-Server/RAM_Disk
+PersistentDir=/home/"$(whoami)"/RPi-MC-Server/Persistent
+StartScript=/home/"$(whoami)"/RPi-MC-Server/start.sh
 
 # Copy files from persistent server folder to RAM Disk server folder.
 sudo cp -R $PersistentDir/* $RAM_DiskDir/
